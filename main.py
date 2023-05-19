b5ea1a226fa0489d5763bd674bcf402c40d79503
sanchitcfc <92146367+sanchitcfc@users.noreply.github.com>
2023-05-19 02:38:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import pandas as pd
 
     
 
+
 def Page_1():
     st.subheader("Monterey")
+    # Add an image
+    
+
+
     sub_page = st.sidebar.selectbox("Select a sub-page", ["Sub-page 1", "Sub-page 2"])
     if sub_page == "Sub-page 1":
         st.write("This is sub-page 1.")
+        video_url = "https://www.youtube.com/watch?v=1ehjVGvjQDw"
+        st.video(video_url)
     elif sub_page == "Sub-page 2":
         st.write("This is sub-page 2.")
 
@@ -17,6 +24,8 @@ def Page_2():
     sub_page = st.sidebar.selectbox("Select a sub-page", ["Sub-page 1", "Sub-page 2"])
     if sub_page == "Sub-page 1":
         st.write("This is sub-page 1.")
+        video_url = "https://www.youtube.com/watch?v=aYZV_y3PaYk"
+        st.video(video_url)
     elif sub_page == "Sub-page 2":
         st.write("This is sub-page 2.")
 
@@ -25,6 +34,8 @@ def Page_3():
     sub_page = st.sidebar.selectbox("Select a sub-page", ["Sub-page 1", "Sub-page 2"])
     if sub_page == "Sub-page 1":
         st.write("This is sub-page 1.")
+        video_url = "https://www.youtube.com/watch?v=vxt5wGaLJRI"
+        st.video(video_url)
     elif sub_page == "Sub-page 2":
         st.write("This is sub-page 2.")
 
@@ -33,6 +44,8 @@ def Page_4():
     sub_page = st.sidebar.selectbox("Select a sub-page", ["Sub-page 1", "Sub-page 2"])
     if sub_page == "Sub-page 1":
         st.write("This is sub-page 1.")
+        video_url = "https://www.youtube.com/watch?v=sW45LPQ07TQ"
+        st.video(video_url)
     elif sub_page == "Sub-page 2":
         st.write("This is sub-page 2.")
 
@@ -42,8 +55,12 @@ def Page_5():
     sub_page = st.sidebar.selectbox("Select a sub-page", ["Sub-page 1", "Sub-page 2"])
     if sub_page == "Sub-page 1":
         st.write("This is sub-page 1.")
+        video_url = "https://www.youtube.com/watch?v=apBi8B1UbuE"
+        st.video(video_url)
     elif sub_page == "Sub-page 2":
         st.write("This is sub-page 2.")
+        image_url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.forbes.com%2Fsites%2Ftomsanderson%2F2023%2F05%2F17%2Flionel-messis-fc-barcelona-return-has-a-set-date-reports%2F&psig=AOvVaw0wBuHjb4git8y0IeTL172w&ust=1684560878819000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCIiy2LnUgP8CFQAAAAAdAAAAABAD"
+        st.image(image_url, caption="Monterey Image", use_column_width=True)
 
 
 
